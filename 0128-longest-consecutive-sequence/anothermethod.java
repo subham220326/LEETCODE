ecf4f5b533552e5e22610afa0b0e4a2054392f7d
Subham Mohanty <subham220326@gmail.com>
2026-07-14 20:27:05 +0530

    Add method to find longest consecutive sequence
    Implement a solution to find the longest consecutive sequence in an array.

---
+++ 0128-longest-consecutive-sequence/anothermethod.java
@@ -0,0 +1,28 @@
+//THIS WORKS IN INTERVIEW AS IT TAKES O(N) TIME COMNPLEXITY HERE IS THE CODE BUT LEETCODE HAS SOME ISSUES REGARDING THAT SO 
+import java.util.Arrays;
+
+class Solution {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+
+        Arrays.sort(nums);
+
+        int longestStreak = 1;
+        int currentStreak = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] == nums[i - 1] + 1) {
+                    currentStreak++;
+                } else {
+                    longestStreak = Math.max(longestStreak, currentStreak);
+                    currentStreak = 1;
+                }
+            }
+        }
+
+        return Math.max(longestStreak, currentStreak);
+    }
+}
